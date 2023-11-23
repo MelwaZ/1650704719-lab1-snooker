1ba99d2b80b1c6e004dc52e0823a3805f3f8f7a2
MelwaZ <kasidet3822@pccst.ac.th>
2023-11-23 16:04:01 +0700

    Shoot Ball

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -10,6 +10,11 @@ public class GameManager : MonoBehaviour
     [SerializeField] private GameObject ballPrefabs;
     [SerializeField] private GameObject[] ballPositions;
 
+    [SerializeField] private GameObject cuvBall;
+    [SerializeField] private GameObject ballLine;
+
+    [SerializeField] private float xInput;
+    [SerializeField] private float force;
 
     void Start()
     {
@@ -24,11 +29,30 @@ public class GameManager : MonoBehaviour
         SetBalls(BallColors.Pink, 6);
         SetBalls(BallColors.Black, 7);
     }
+    void Update()
+    {
+        RotateBall();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            ShootBall();
+        }
+    }
 
     void SetBalls(BallColors color,int pos)
     {
         GameObject ball =  Instantiate(ballPrefabs,ballPositions[pos].transform.position,Quaternion.identity);
         Ball b = ball.GetComponent<Ball>();
         b.SetColorAndPont(color);
+    }
+    void RotateBall()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        cuvBall.transform.Rotate(new Vector3(0f, xInput/10, 0f));
+    }
+    void ShootBall()
+    {
+        Rigidbody rd = cuvBall.GetComponent<Rigidbody>();
+        rd.AddRelativeForce(Vector3.forward * force, ForceMode.Impulse);
+        ballLine.SetActive(false);
     }
 }
